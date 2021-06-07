dbc29ed5027cd2c9771fa4d5d48f58c42f7b003a
Tim Player <playert@oregonstate.edu>
2021-06-07 12:48:08 -0700

    Add SDF evaluation to training data

--- generate_data.py
+++ generate_data.py
@@ -52,7 +52,21 @@ def collect_examples(shape):
                 break
         
         te = TrainingExample(robx=x, roby=y, shape=shape)
-        examples.append(te)
+
+        # Sample SDF points
+        xs = np.random.uniform(
+            *params.DataGen.VIEW_RANGE[:2], 
+            size=(params.DataGen.N_SDF_QUERIES,)
+            )
+        ys = np.random.uniform(
+            *params.DataGen.VIEW_RANGE[2:], 
+            size=(params.DataGen.N_SDF_QUERIES,)
+            )
+        query_pts = np.column_stack([xs, ys])
+        dists = te.sdf(query_pts)
+
+        view = (te.scan_pts, query_pts, dists)
+        examples.append(view)
     return examples
 
 print("Rendering training examples from polygons.")
@@ -67,7 +81,7 @@ with Pool(cpu_count()-1) as p:
 
 examples = list(itertools.chain(*examples))
 
-# Each example is a TrainingExample object. A TrainingExample encodes a scene with a randomly positioned robot, and it has the following state and behavior:
+# A TrainingExample encodes a scene with a randomly positioned robot, and it has the following state and behavior:
 # robx, roby    : the x and y position of the robot
 # shape         : the Polygon object (in the global frame), centered at (0,0)
 # robang        : the angle from the robot to the center of the visible region of the shape
@@ -75,9 +89,31 @@ examples = list(itertools.chain(*examples))
 # sdf(x)        : a vectorized function converting an ndarray of coordinates (x,y) expressed in the robot local frame, to distance from the object. This function is used to evaluate the ground truth SDF.
 # grasp_quality(theta, b) : a function giving the quality of a grasp with angle theta and offset b, on the object's `shape` attribute.
 
-## Write training data
-print("Pickling training examples.")
-pickle.dump(examples, open(
-    output_dir+"/examples.p", "wb"))
+# Our data consists of tuples containing three ndarrays:
+# scan_pts      : an (N_SCAN_POINTS,2) ndarray of coordinates corresponding to the local-frame cartesian locations obtained by casting rays from the robot to the shape. The rays span the entire visible portion of the shape.
+# query_pts     : an (N_SDF_QUERIES, 2) ndarray of uniformly sampled points from the ROBOT_RANGE to sample SDF
+# dists         : the SDF value associated with each query point
 
-print("Complete.")
+
+## Partition into train, test, and validation sets and shuffle data.
+print("Partitioning and shuffling data.")
+split1 = int(0.8 * len(examples))
+split2 = int(0.9 * len(examples))
+
+train_examples = examples[:split1]
+val_examples   = examples[split1:split2]
+test_examples  = examples[split2:]
+
+import random
+random.seed(535)
+random.shuffle(train_examples)
+random.shuffle(val_examples)
+random.shuffle(test_examples)
+
+
+## Write data into its home
+print("Writing data to output directory.")
+pickle.dump(train_examples, open(
+    output_dir+"/train/train_examples.p", "wb"))
+pickle.dump(val_examples, open(output_dir+"/val/val_examples.p", "wb"))
+pickle.dump(test_examples, open(output_dir+"/test/test_examples.p", "wb"))
