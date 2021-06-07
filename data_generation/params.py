d43508bc3328a8b07bc44f5a7bc91610422e6cf2
Tim Player <playert@oregonstate.edu>
2021-06-07 12:48:20 -0700

    Add view range to params

--- data_generation/params.py
+++ data_generation/params.py
@@ -14,7 +14,9 @@ class TrainingExample:
     FRICTION_COEFFICIENT = 0.3
 
 class DataGen:
-    N_POLYGONS = 5*int(1e3)        # number of different shapes
+    N_POLYGONS = int(1e3)        # number of different shapes
     N_PERSPECTIVES = int(1e2)    # number of different robot perspectives
     MIN_DISTANCE = 1        # minimum distance from robot loc to shape
     ROBOT_RANGE = [-10, 10, -10, 10]   # bounds min_x, max_x, min_y, max_y for robot
+    VIEW_RANGE = [MIN_DISTANCE, 15, -5, 5]
+    N_SDF_QUERIES = 1000
