58c3581443493525d60165fc79e07da5fe04f1b4
Tim Player <playert@oregonstate.edu>
2021-06-06 22:59:17 -0700

    Add data generation for shape recon

---
+++ data_generation/params.py
@@ -0,0 +1,19 @@
+"""
+params.py
+Global parameters for data generation code.
+
+Tim Player playertr@oregonstate.edu June 1, 2021
+"""
+from math import pi
+
+class Robot:
+    N_SCAN_POINTS = int(100)
+    SCAN_HALF_ANGLE = 30 * pi/180 # radians
+    SCAN_RANGE = 99999
+    FOV_REDUCTION_MARGIN = 0.0001 # radians
+
+class DataGen:
+    N_POLYGONS = 5*int(1e3)        # number of different shapes
+    N_PERSPECTIVES = int(1e2)    # number of different robot perspectives
+    MIN_DISTANCE = 1        # minimum distance from robot loc to shape
+    ROBOT_RANGE = [-10, 10, -10, 10]   # bounds min_x, max_x, min_y, max_y for robot
--- generate_data.py
+++ generate_data.py
@@ -2,108 +2,81 @@
 generate_data.py
 Script for generating training data.
 
+Training data consists of single-view LIDAR scans producing local-frame cartesian coordinates, obtained by casting N_SCAN_POINTS rays that evenly land on the shape surface. The shapes are all random pentagons.
+
+For each shape, N_PERSPECTIVES different robot positions are chosen.
+
+Ground truth is provided as a matrix of vertices.
+
 Tim Player playertr@oregonstate.edu May 23,2021
 """
 
 from data_generation.shape_generator import PolygonGen
-from data_generation.point_sampler import UniformPointSampler
-from data_generation.gp import VanillaGP
+from data_generation.training_ex import TrainingExample
+import data_generation.params as params
+from shapely.geometry import Point
 import numpy as np
-import pickle
-from multiprocessing import Pool, cpu_count
 from tqdm import tqdm
+from multiprocessing import Pool, cpu_count
+import itertools
 import argparse
+import pickle
 
 ## Parse command line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('--output_dir', default='network/data',
+parser.add_argument('--output_dir', default='data',
                     help="Directory into which to write the dataset")
-parser.add_argument('--num_examples', type=int, default=100000,
-                    help="How many examples to generate")
 
+print("Generating Data. See data_generation/params.py to modify data properties.")
 args = parser.parse_args()
 output_dir = args.output_dir
 
-## Define constants
-N_POLYGONS = args.num_examples   # Number of polygons/training examples to generate data for
-N_GP_TRAINING_PTS = 500 # Number of points sampled from each polygon to train GP
-N_SURFACE_RAYS = 100    # Number of evenly spaced rays used to represent zero level set
-
 ## Generate random polygons
+print("Generating random polygons.")
 pg = PolygonGen()
-shapes = pg.get_shapes(5, N_POLYGONS)
+shapes = pg.get_shapes(5, params.DataGen.N_POLYGONS)
 
-## Retrieve zero-level surface points from GPs trained on these random polygons
+## Collect rendered views of each polygon as well as SDF fields
+def collect_examples(shape):
+    examples = []
+    for _ in range(params.DataGen.N_PERSPECTIVES):
+        min_x, max_x, min_y, max_y = params.DataGen.ROBOT_RANGE
 
-# First, we define a function that samples points from a polygon's surface,
-# trains a GP to approximate the polygon, and then identifies the "zero
-# level set".
+        # Sample workspace uniformly for robot location, but filter by distance
+        # from shape
+        while True:
+            x = np.random.rand() * (max_x-min_x) - (max_x-min_x)/2
+            y = np.random.rand() * (max_y-min_y) - (max_y-min_y)/2
 
-def training_data_and_surface(shape):
-    """Train a GP on this shape and return the sampled input points, scalar
-    observations `y` for each input point, and the zero level set denoting the
-    object's surface.
+            if shape.exterior.distance(Point(x,y)) > params.DataGen.MIN_DISTANCE:
+                break
+        
+        te = TrainingExample(robx=x, roby=y, shape=shape)
+        examples.append(te)
+    return examples
 
-    The zero level set is defined as the distance from the origin to the SDF
-    zero along many evenly-spaced rays from the origin.
-
-    Args: shape (Polygon): shape to train GP on
-
-    Returns: (tuple of ndarrays): pts is a 
-    """
-
-    # Sample points from the shape surface to train the GP
-    ps = UniformPointSampler()
-    pts = ps.sample_points(shape, N_GP_TRAINING_PTS)
-    y = np.zeros((pts.shape[0]))
-
-    # Set a point at the origin to have negative SDF
-    pts[0] = [0, 0]
-    y[0] = -1
-
-    # Train the GP and the distance to the regressed surface along
-    # evenly-spaced rays from the origin.
-    gp = VanillaGP(noise=0.02, l=0.5, sig_var=1)
-    gp.train(pts, y)
-    surface_pts = gp.get_surface(N_SURFACE_RAYS)
-
-    return (pts, y, surface_pts)
-
-# It is expensive to retrieve the training data and surface points from all
-# of the shapes. To speed this up, we will use a multiprocessing.Pool to
-# compute the training data and surface points from shapes in each chunk in
-# parallel.
-
-# tqdm gives us a progress bar.
-
+print("Rendering training examples from polygons.")
+# Collect training examples from shapes in parallel
 with Pool(cpu_count()-1) as p:
     examples = list(
         tqdm(
-            p.imap(training_data_and_surface, shapes),
+            p.imap(collect_examples, shapes),
             total=len(shapes)
         )
     )
 
-# `examples` is a list of tuples. Each tuple has three ndarrays: `pts`, `y`,
-# and `surface_pts`.
+examples = list(itertools.chain(*examples))
 
-print("Finished retrieving ground truth zero level sets and training data.")
+# Each example is a TrainingExample object. A TrainingExample encodes a scene with a randomly positioned robot, and it has the following state and behavior:
+# robx, roby    : the x and y position of the robot
+# shape         : the Polygon object (in the global frame), centered at (0,0)
+# robang        : the angle from the robot to the center of the visible region of the shape
+# scan_pts      : an (N_SCAN_POINTS,2) ndarray of coordinates corresponding to the local-frame cartesian locations obtained by casting rays from the robot to the shape. The rays span the entire visible portion of the shape.
+# sdf(x)        : a vectorized function converting an ndarray of coordinates (x,y) expressed in the robot local frame, to distance from the object. This function is used to evaluate the ground truth SDF.
 
+## Write training data
+print("Pickling training examples.")
+pickle.dump(examples, open(
+    output_dir+"/examples.p", "wb"))
 
-## Shuffle data and partition into train, test, and validation sets
-import random
-random.seed(112)
-random.shuffle(examples)
-
-split1 = int(0.8 * len(examples))
-split2 = int(0.9 * len(examples))
-
-train_examples = examples[:split1]
-val_examples   = examples[split1:split2]
-test_examples  = examples[split2:]
-
-## Write data into its home in the network/ folder
-pickle.dump(train_examples, open(
-    output_dir+"/train_sdf/sdf_train_examples.p", "wb"))
-pickle.dump(val_examples, open(output_dir+"/val_sdf/sdf_val_examples.p", "wb"))
-pickle.dump(test_examples, open(output_dir+"/test_sdf/sdf_test_examples.p", "wb"))
+print("Complete.")
