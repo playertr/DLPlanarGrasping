d9c9591db800d5c7f5c06a3c3e83bbe5cee4dbe6
Tim Player <playert@oregonstate.edu>
2021-06-07 12:48:50 -0700

    Modify data loader for SDF data

--- network/model/data_loader.py
+++ network/model/data_loader.py
@@ -21,21 +21,23 @@ class SDFDataset(Dataset):
         filenames = [os.path.join(data_dir, f) for f in filenames if f.endswith('.p')]
 
         if len(filenames) != 1:
-            print("Funny number of data files in this directory.")
+            print(f"Funny number of data files in this directory. Should be 1, is {len(filenames)}")
         
         f = filenames[0]
 
-        # Load data from 100K shapes.
-        # examples is a list of lists, one per training example.
-        # Each list contains a 
-        #   (500,2) ndarray of points           Xtrain
-        #   (500,)  ndarray of function values  ytrain
-        #   (100,)  ndarray of SDF zero points  r
+        # Load data from 1000 shapes.
+        # examples is a list of tuples, one per training shape.
+        # Each tuple contains three ndarrays:
+        # scan_pts      : an (N_SCAN_POINTS,2) ndarray of coordinates corresponding to the local-frame cartesian locations obtained by casting rays from the robot to the shape. The rays span the entire visible portion of the shape.
+        # query_pts     : an (N_SDF_QUERIES, 2) ndarray of uniformly sampled points from the ROBOT_RANGE to sample SDF
+        # dists         : the SDF value associated with each query point
         self.examples = pickle.load(open( f, "rb" ))
+        self.sdf_queries_per_shape = self.examples[0][1].shape[0]
 
     def __len__(self):
         # return size of dataset
-        return len(self.examples)
+        
+        return len(self.examples) * self.sdf_queries_per_shape
 
     def __getitem__(self, idx):
         """
@@ -45,18 +47,24 @@ class SDFDataset(Dataset):
             idx: (int) index in [0, 1, ..., size_of_dataset-1]
 
         Returns:
-            image: (Tensor) transformed image
-            label: (int) corresponding label of image
+            input: (Tensor) concatenation of scan_pts with query (x,y)
+            distance: (float) corresponding distance from query to surface
         """
-        Xtrain, ytrain, r = self.examples[idx] # See description above.
-
         
+        shape_idx = int(idx / self.sdf_queries_per_shape)
+        pt_idx = idx % self.sdf_queries_per_shape
+
+        scan_pts, query_pts, dists = self.examples[shape_idx] # See description above.
+        query_pt = query_pts[pt_idx]
+        dist = dists[pt_idx]
         # We want:
         # INPUT: (500,3) concatenation of Xtrain, ytrain for this shape
         # OUTPUT: (100,) ndarray of SDF zero points
 
-        data = np.column_stack([Xtrain, ytrain])
-        return data, r
+        data = np.vstack([
+            scan_pts, query_pt
+            ])
+        return data, dist
 
 
 def fetch_dataloader(types, data_dir, params):
@@ -75,7 +83,7 @@ def fetch_dataloader(types, data_dir, params):
 
     for split in ['train', 'val', 'test']:
         if split in types:
-            path = os.path.join(data_dir, "{}_sdf".format(split))
+            path = os.path.join(data_dir, "{}".format(split))
 
             # use the train_transformer if training data, else use eval_transformer without random flip
             if split == 'train':
