68cc07a43c545f332d9676b254a1f9dcdc15abbd
Tim Player <playert@oregonstate.edu>
2021-06-11 14:18:52 -0700

    Scale PointNet layers

--- network/model/net.py
+++ network/model/net.py
@@ -10,19 +10,34 @@ class Tnet(nn.Module):
       super().__init__()
       self.k=k
 
-      self.conv1 = nn.Conv1d(k,64,1)
-      self.conv2 = nn.Conv1d(64,128,1)
-      self.conv3 = nn.Conv1d(128,1024,1)
+    #   self.conv1 = nn.Conv1d(k,64,1)
+    #   self.conv2 = nn.Conv1d(64,128,1)
+    #   self.conv3 = nn.Conv1d(128,1024,1)
 
-      self.fc1 = nn.Linear(1024,512)
-      self.fc2 = nn.Linear(512,256)
-      self.fc3 = nn.Linear(256,k*k)
+    #   self.fc1 = nn.Linear(1024,512)
+    #   self.fc2 = nn.Linear(512,256)
+    #   self.fc3 = nn.Linear(256,k*k)
 
-      self.bn1 = nn.BatchNorm1d(64)
-      self.bn2 = nn.BatchNorm1d(128)
-      self.bn3 = nn.BatchNorm1d(1024)
-      self.bn4 = nn.BatchNorm1d(512)
-      self.bn5 = nn.BatchNorm1d(256)
+    #   self.bn1 = nn.BatchNorm1d(64)
+    #   self.bn2 = nn.BatchNorm1d(128)
+    #   self.bn3 = nn.BatchNorm1d(1024)
+    #   self.bn4 = nn.BatchNorm1d(512)
+    #   self.bn5 = nn.BatchNorm1d(256)
+      SCALE=16
+      self.conv1 = nn.Conv1d(k,int(64/SCALE),1)
+      self.conv2 = nn.Conv1d(int(64/SCALE),int(128/SCALE),1)
+      self.conv3 = nn.Conv1d(int(128/SCALE),int(1024/SCALE),1)
+
+      self.fc1 = nn.Linear(int(1024/SCALE),int(512/SCALE))
+      self.fc2 = nn.Linear(int(512/SCALE),int(256/SCALE))
+      self.fc3 = nn.Linear(int(256/SCALE),k*k)
+
+      self.bn1 = nn.BatchNorm1d(int(64/SCALE))
+      self.bn2 = nn.BatchNorm1d(int(128/SCALE))
+      self.bn3 = nn.BatchNorm1d(int(1024/SCALE))
+      self.bn4 = nn.BatchNorm1d(int(512/SCALE))
+      self.bn5 = nn.BatchNorm1d(int(256/SCALE))
+      
        
 
    def forward(self, input):
@@ -48,15 +63,24 @@ class Transform(nn.Module):
    def __init__(self):
         super().__init__()
         self.input_transform = Tnet(k=2)
-        self.feature_transform = Tnet(k=64)
+        # self.feature_transform = Tnet(k=64)
 
-        self.conv1 = nn.Conv1d(2,64,1)
-        self.conv2 = nn.Conv1d(64,128,1)
-        self.conv3 = nn.Conv1d(128,1024,1)
+        # self.conv1 = nn.Conv1d(2,64,1)
+        # self.conv2 = nn.Conv1d(64,128,1)
+        # self.conv3 = nn.Conv1d(128,1024,1)
        
-        self.bn1 = nn.BatchNorm1d(64)
-        self.bn2 = nn.BatchNorm1d(128)
-        self.bn3 = nn.BatchNorm1d(1024)
+        # self.bn1 = nn.BatchNorm1d(64)
+        # self.bn2 = nn.BatchNorm1d(128)
+        # self.bn3 = nn.BatchNorm1d(1024)
+        SCALE=2
+        self.feature_transform = Tnet(k=int(64/SCALE))
+        self.conv1 = nn.Conv1d(2,int(64/SCALE),1)
+        self.conv2 = nn.Conv1d(int(64/SCALE),int(128/SCALE),1)
+        self.conv3 = nn.Conv1d(int(128/SCALE),int(1024/SCALE),1)
+       
+        self.bn1 = nn.BatchNorm1d(int(64/SCALE))
+        self.bn2 = nn.BatchNorm1d(int(128/SCALE))
+        self.bn3 = nn.BatchNorm1d(int(1024/SCALE))
        
    def forward(self, input):
         matrix2x2 = self.input_transform(input)
@@ -79,14 +103,25 @@ class PointNet(nn.Module):
     def __init__(self, classes = 1):
         super().__init__()
         self.transform = Transform()
-        self.fc1 = nn.Linear(1024, 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, classes)
+        # self.fc1 = nn.Linear(1024, 512)
+        # self.fc2 = nn.Linear(512, 256)
+        # self.fc3 = nn.Linear(256, classes)
         
 
-        self.bn1 = nn.BatchNorm1d(512)
-        self.bn2 = nn.BatchNorm1d(256)
-        self.dropout = nn.Dropout(p=0.3)
+        # self.bn1 = nn.BatchNorm1d(512)
+        # self.bn2 = nn.BatchNorm1d(256)
+
+        SCALE=2
+        self.fc1 = nn.Linear(int(1024/SCALE), int(512/SCALE))
+        self.fc2 = nn.Linear(int(512/SCALE), int(256/SCALE))
+        self.fc3 = nn.Linear(int(256/SCALE), classes)
+        
+
+        self.bn1 = nn.BatchNorm1d(int(512/SCALE))
+        self.bn2 = nn.BatchNorm1d(int(256/SCALE))
+
+
+        self.dropout = nn.Dropout(p=0)
         self.logsoftmax = nn.LogSoftmax(dim=1)
 
     def forward(self, x):
